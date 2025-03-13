130a3d60ecc09c7ba70ed1c2abf017156db00805
Bartosz Lesiński <bartosz.lesinski@interia.pl>
2025-03-13 23:21:02 +0100

    continuation or exit

--- Program.cs
+++ Program.cs
@@ -11,8 +11,26 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             var FizzBuzz = new FizzBuzz();
-            var result = FizzBuzz.CheckNumber();
-            Console.WriteLine(result);
+            string answer;
+            bool continuation;
+            do
+            {
+                var result = FizzBuzz.CheckNumber();
+                Console.WriteLine(result);
+                Console.WriteLine("Wprowadź 'K' by zakończyć, lub inny znak by kontunuować");
+                answer = Console.ReadLine();
+                if (answer == "K")
+                {
+                    continuation = false;
+                }
+                else
+                {
+                    continuation = true;
+                }
+
+            } while (continuation);
+
         }
+
     }
 }
